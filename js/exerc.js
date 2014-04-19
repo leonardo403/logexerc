8f429157808f8132d90a51edc052fdaa847cc4b4
leonardo lima <leonardo403@gmail.com>
2014-04-19 02:26:54 -0300

    alteracao na maneira,de add valores na tabela utilizando somente JS para fazer o add.

--- js/exerc.js
+++ js/exerc.js
@@ -1,13 +1,55 @@
+
 function add() {
-	var valor = document.getElementById("frm");
-	var dados = "";
-	
-for(var i=0;i<valor.length;i++)
-  {
-  	dados = dados + valor.elements[i].value;
-  	//document.write(valor.length);
+	//var  	
+ var tempo = document.getElementById('tempo');
+ var tipo  = document.getElementById('tipo');
+ var data  = document.getElementById('data');
+ 	
+ 	//try catch
+ 	try {
+    localStorage.setItem('tempo', tempo.value);
+    localStorage.setItem('tipo', tipo.value);
+    localStorage.setItem('data', data.value);
+    
+    tempo.value = '';
+    tipo.value = '';
+    data.value = '';
+
+	document.getElementById('mytempo').value = localStorage.getItem('tempo');
+    document.getElementById('mytipo').value  = localStorage.getItem('tipo');
+    document.getElementById('mydata').value = localStorage.getItem('data');
+	    
+}
+catch (e) {
+    if (e == QUOTA_EXCEEDED_ERR) {
+        console.log("Error: Local Storage limit exceeds.");
+    }
+    else {
+        console.log("Error: Saving to local storage.");
+    }
   }
-	document.getElementById("log").innerHTML=dados;
 }
 
-	
+//remove local storage key
+function rm() {
+	console.log('Remove key of local Storage.');
+	localStorage.removeItem('tempo');
+	localStorage.removeItem('tipo');
+	localStorage.removeItem('data');
+}
+
+//clearing local storage
+function clear() {
+	console.log('clearing localStorage.');
+	localStorage.clear();
+}
+
+//checkout if exists 
+function load(){
+	console.log("Page loading finished.");
+	if (typeof(Storage) != "undefined") 
+		{console.log("Your browser support web storage.");
+		} else{
+			console.log("Your browser not support web storage.");
+		}
+}
